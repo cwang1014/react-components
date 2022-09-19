c6b4e764cc6f7421f83af89600ee26b7b94f42a3
Curtis Wang <curtis.wang10@gmail.com>
2022-09-19 11:13:50 -0700

    yesssssssssssss

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+function GroceryList(props) {
+  return (
+    <div>
+      <h2>My Todo List</h2>
+      <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe maybe sleep']}/>
+    </div>
+  );
+}
+
+function TodoListItem(props) {
+  const [isDone, setIsDone] = React.useState(false);
+  const [isHovering, setIsHovering] = React.useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  }
+
+  const style = {
+    textDecoration: isDone ? 'line-through' : 'none',
+    fontWeight: isHovering ? 'bold' : 'normal',
+  };
+
+  return (
+    <li
+      style={style}
+      onClick={() => setIsDone(!isDone)}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      {props.todo}
+    </li>
+  );
+}
+
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map((todo) => (
+        <TodoListItem todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
